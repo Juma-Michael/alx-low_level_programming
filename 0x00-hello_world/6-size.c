2ff146fd20b38884e17aada5ec2be21c2e5a7191
Juma-Michael <michaeljuma86@gmail.com>
2023-09-07 19:28:35 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,19 @@
 
 /**
  *
- * main - the function itself
+ * main - Main function
  *
- * Return: expected to be 0
+ * Return: 0 when successful
  */
 
-main(void)
+int main(void)
 {
 	printf("Size of char: %lu byte(s)", sizeof(char));
 	printf("Size of an int: %lu byte(s)", sizeof(int));
 	printf("Size of a long: %lu byte(s)", sizeof(long int));
 	printf("Size of a long long int: %lu byte(s)", sizeof(long long int));
 	printf("Size of float: %lu byte(s)", sizeof(float));
+
+
 	return (0);
 }
