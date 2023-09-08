2d5e1067555f5a13ce2f4e39837b6c6f87b621b6
Juma-Michael <michaeljuma86@gmail.com>
2023-09-07 17:36:15 -0700

    Puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
-#!/bin/bash
+
 #include <stdio.h>
+
+/**
+ *main  - the starting  point
+ *
+ *Return: 0 always
+ */
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n")
-		return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
