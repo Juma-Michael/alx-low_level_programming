8f9b64fecc165a4e70c44b9c893ae88375dc236c
Juma-Michael <michaeljuma86@gmail.com>
2023-09-10 21:54:08 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,23 +5,18 @@
  */
 int main(void)
 {
-int i;
-int j;
-int k;
-for (i = 0; i < 10; i++)
+int first;
+int second;
+for (first = 0; first < 10; first++)
 {
-for (j = i + 1; j < 10; j++)
+for (second = first + 1; second < 10; second++)
 {
-for (k = j + 1 ; k < 10; k++)
-{
-	putchar(i + '0');
-	putchar(j + '0');
-	putchar(k + '0');
-if (i != 7 || j != 8 || k != 9)
+	putchar(first + '0');
+	putchar(second + '0');
+if (first != 8 || second != 9)
 {
 	putchar(',');
 	putchar(' ');
-}
 }
 }
 }
