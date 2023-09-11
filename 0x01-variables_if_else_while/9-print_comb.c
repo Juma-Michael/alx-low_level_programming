48a5c3fba21b0175c7847701eaa034da447d437a
Juma-Michael <michaeljuma86@gmail.com>
2023-09-10 22:08:01 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,14 @@
 int main(void)
 {
 int i;
-for (i = 0; i <= 10; i++)
+for (i = 0; i < 10; i++)
 {
 	putchar(i + '0');
+if (i < 9)
+{
 	putchar(',');
 	putchar(' ');
-	i++;
+}
 }
 	putchar('\n');
 	return (0);
